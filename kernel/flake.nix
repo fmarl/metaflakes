5152cbce0c847aa65dca1019711adcb0bb4367b5
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-04-29 21:45:11 +0200

    Implement kernel and rust flake

---
+++ kernel/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Linux development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs;
+            [
+              git
+              gdb
+              qemu
+              pahole
+              flex
+              bison
+              bc
+              pkg-config
+              elfutils
+              openssl.dev
+              llvmPackages.clang
+              (python3.withPackages (ps: with ps; [
+                GitPython
+                ply
+              ]))
+              codespell
+
+              # static analysis
+              flawfinder
+              cppcheck
+              sparse
+            ];
+        };
+    };
+}
