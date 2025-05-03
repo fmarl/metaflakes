5ab15f75606f1529c331a7e1d9aad466214e580e
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-05-03 13:04:17 +0200

    Switch from gcc to clang for kernel

--- kernel/default.nix
+++ kernel/default.nix
@@ -1,11 +1,28 @@
 { system, nixpkgs }:
 let
   pkgs = import nixpkgs { inherit system; };
+  lib = nixpkgs.lib;
+  clangVersion = lib.versions.major pkgs.llvmPackages.libclang.version;
+  cc = pkgs.llvmPackages.stdenv.cc.override {
+    bintools = pkgs.llvmPackages.bintools;
+    extraBuildCommands = ''
+      substituteInPlace "$out/nix-support/cc-cflags" --replace " -nostdlibinc" ""
+      echo " -resource-dir=${pkgs.llvmPackages.libclang.lib}/lib/clang/${clangVersion}" >> $out/nix-support/cc-cflags
+    '';
+  };
 in
 pkgs.mkShell {
   name = "kernel-dev-shell";
+  hardeningDisable = [ "all" ];
+  LLVM = 1;
+
   buildInputs = with pkgs; [
     git
+    (python3.withPackages (ps: with ps; [
+      GitPython
+      ply
+    ]))
+
     gdb
     qemu
     pahole
@@ -15,17 +32,20 @@ pkgs.mkShell {
     pkg-config
     elfutils
     openssl.dev
-    llvmPackages.clang
-    (python3.withPackages (ps: with ps; [
-      GitPython
-      ply
-    ]))
-    codespell
     perl
 
+    # LLVM=1
+    cc.cc
+    llvmPackages.bintools-unwrapped
+    llvmPackages.lld
+
+    clang-tools
     # static analysis
+    codespell
     flawfinder
     cppcheck
     sparse
+    valgrind
+    aflplusplus
   ];
 }
