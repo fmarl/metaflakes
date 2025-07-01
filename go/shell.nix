8a3ed1e7b605b58b74855bdcc03ebd427205c9fb
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-07-01 14:27:47 +0200

    Update shell.nix

--- go/shell.nix
+++ go/shell.nix
@@ -1,9 +1,16 @@
 { system, nixpkgs }:
 let pkgs = import nixpkgs { inherit system; };
 in pkgs.mkShell {
+  CGO_ENABLED = 0;
   name = "go-dev-shell";
 
-  buildInputs = with pkgs; [ pkgs.go pkgs.gopls pkgs.golangci-lint ];
+  buildInputs = with pkgs; [
+    pkgs.nixfmt
+    pkgs.go
+    pkgs.gopls
+    pkgs.golangci-lint
+    pkgs.delve
+  ];
 
   shellHook = ''
     echo "Let's GO!"
